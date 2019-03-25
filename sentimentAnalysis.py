90d143dbd67ae9c2f91852f7e5e8ef9a3a103962
joel <joel.tay.2016@sis.smu.edu.sg>
2019-03-25 20:29:06 +0800

    commit sentiment analysis part 2

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,7 +1,5 @@
 import nltk, re, csv,time
 from gensim import corpora
-from random import shuffle
-stop_list = nltk.corpus.stopwords.words('english')
 # The following list is to further remove some frequent words in SGNews.
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -9,10 +7,12 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import svm
-from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 import pickle
-startTime = time.time()
+from textblob import TextBlob
 
+startTime = time.time()
+stop_list = nltk.corpus.stopwords.words('english')
 field='Content'
 labelField='polarity'
 docs=[]
@@ -23,20 +23,23 @@ with open("data/preprocessed_reviewinfo.csv",encoding='utf-8') as csvfile:
             counter =1
             for row in reader:
                 sampleData.append(row)
-            shuffle(sampleData) # Shuffle the data around
             for row in sampleData:
                 label.append(row[labelField])        
                 docs.append(row[field])
 
+#using textblob as a lexicon
+"""
+for i in range(0,5):
+    print(TextBlob(docs[i]).sentiment)
+    print(docs[i])"""
 
 print('Finished reading sentences from the training data file. Time: ', time.time()-startTime )
 
 
 print("finish converting to vector")
-x_train, x_test, y_train, y_test = train_test_split(docs,label,test_size =0.2)
-count = CountVectorizer()
+x_train, x_test, y_train, y_test = train_test_split(docs,label,test_size =0.3, random_state=50)
+count = CountVectorizer(max_features=1000, lowercase=True, stop_words= 'english', ngram_range=(1,1),analyzer = "word")
 temp = count.fit_transform(x_train)
-
 tfidf = TfidfTransformer()
 temp2 = tfidf.fit_transform(temp)
 
@@ -62,16 +65,21 @@ clf = svm.LinearSVC()
 model= clf.fit(temp2,y_train)
 filename = 'model_sentiment/svm_model.pk'
 '''
-print(type(x_test))
-print(x_test[:3])
+
 prediction = model.predict(tfidf.transform(count.transform(x_test)))
-print(prediction[:5])
-print(y_test[:5])
+
 print("Model accuracy : " + str(np.mean(prediction==y_test)))
 
 pickle.dump(model, open(filename, 'wb'))
+pickle.dump(tfidf, open('model_sentiment/tfidf_trans.pk', 'wb'))
+pickle.dump(count, open('model_sentiment/count_vert.pk', 'wb'))
+
 
 print('\nClasification report:\n', classification_report(y_test, prediction))
 print('\nConfussion matrix:\n',confusion_matrix(y_test, prediction)  )
     
 print("Time taken: " + str(time.time() - startTime))
+
+z_test = [input("What is your review? ")]
+prediction = model.predict(tfidf.transform(count.transform(z_test)))
+print("prediction is:" + prediction[0])
--- sentimentAnalysis2.py
+++ sentimentAnalysis2.py
@@ -9,12 +9,13 @@ filename = 'model_sentiment/logistic_regression_model.pk'
 #filename = 'model_sentiment/nb_model.pk
 #filename = 'model_sentiment/svm_model.pk'
 
-tfidf = TfidfTransformer()
-count = CountVectorizer()
+tfidf = pickle.load(open('model_sentiment/tfidf_trans.pk','rb'))
+count = pickle.load(open('model_sentiment/count_vert.pk','rb'))
 
 # load the model from disk
 model = pickle.load(open(filename, 'rb'))
-x_test = ["this is wonderful"]
-prediction = model.predict(tfidf.transform(count.transform(x_test)))
 
-print(prediction)
+z_test = [input("What is your review? ")]
+prediction = model.predict(tfidf.transform(count.transform(z_test)))
+
+print("prediction is:" + "positive rating" if prediction[0] == "1" else "prediction is: negative rating",)

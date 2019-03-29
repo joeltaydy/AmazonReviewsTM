8528ff35cdb3fab135a391926e236d919c0518d6
Ryan Yang <ryanyang.2016@sis.smu.edu.sg>
2019-03-29 19:10:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ import numpy as np
 from dash.dependencies import Output, State, Input, Event
 
 categories = ['Handphones', 'Laptops', 'Desktops']
+tableView = pd.DataFrame(columns=['review', 'category', 'sentiment'])
+
+def generateTable(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -36,6 +49,11 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
             html.Button(id='submit-button', type='submit', children='Submit'),
             html.Div(id='output_div')
     ]),
+
+    # html.Div(children = [
+    #     html.H4(children='Output'),
+    #     generateTable(tableView)
+    # ]),
 
     dcc.Graph(
         id='sentiment analysis',
@@ -101,26 +119,34 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     # )
 ])
 
-
+# tableView = pd.DataFrame(columns=['review', 'category', 'sentiment'])
 @app.callback(Output('output_div', 'children'),
             [],
             [State('review', 'value')],
             [Event('submit-button', 'click')]
 )
 
-def update_output(input_value):
-        print('input: ' + input_value)
+def update_output(review):
+    # tableView = pd.DataFrame(columns=['review', 'category', 'sentiment'])
+    # run review through sentiment
+    sentiment = runModelSentiment(review)
+    # # run review through category classification 
+    category = runModelCategoryClassification(review)
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+    # print('table view: ')
+    # print(tableView.columns)
+    
+    row = pd.Series([review, category, sentiment], index=tableView.columns)
+    view = tableView.append(row, ignore_index = True)
+    print(row)
+    return generateTable(view)
 
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
-    )
+def runModelSentiment(input_value):
+    return 'Positive'
+
+
+def runModelCategoryClassification(input_value):
+    return 'test class'
 
 if __name__ == '__main__':
     app.run_server(debug=True)

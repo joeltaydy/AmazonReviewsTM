93f90325abf64a331308e5b6b38575d004cff3f0
JingYuan-SMU <jypeh.2015@smu.edu.sg>
2019-03-31 14:39:52 +0800

    Update app.py
    a

--- app.py
+++ app.py
@@ -359,7 +359,46 @@ def runModelSentiment(input_value):
 
 
 def runModelCategoryClassification(input_value):
-    return 'cameras'
+    import sklearn
+    from sklearn.model_selection import train_test_split,ShuffleSplit
+    from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn import model_selection, naive_bayes, svm
+
+    import numpy as np
+    from sklearn import svm
+    import pickle
+    import pandas as pd
+
+    # Load the processed reviews
+    main_df = pd.read_csv('data/processed_reviews.csv')
+
+    # Load the classifier
+    classifier_saved = open("model_classification/CategoryClassifier.pickle", "rb")
+    classifier = pickle.load(classifier_saved)
+    classifier_saved.close()
+
+    #Load the saved classifier 
+    classifier_saved = open("model_classification/TFIDF_Reviews_Category.pickle", "rb") #binary read
+    TFIDF_vect = pickle.load(classifier_saved)
+    classifier_saved.close()
+
+    #process input
+    review_holder = np.array([input_value])
+    review_holder_1 = pd.Series(review_holder)
+
+    # Transform reviews into TFIDF
+    review_TFIDF = TFIDF_vect.transform(review_holder_1)
+    prediction = classifier.predict(test_TDIDF)
+
+    if prediction[0] == 0:
+        return "Camera"
+    elif prediction[0] == 1:
+        return "Laptop"
+    else
+        return "Mobile Phone"
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
